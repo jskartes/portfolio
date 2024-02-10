8128016bc62973dbdb728f0f487864443a91a80a
jskartes <joshuakartes@gmail.com>
2024-02-10 11:43:36 -0700

    use Jura font

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <div className='App'>
       <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div className='Home'>
+      <div className='home-hero'>
+        <span className='home-name'>Joshua Stephen Kartes</span>
+        <span className='home-role'>
+          Software Engineer & Full-Stack Developer
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
