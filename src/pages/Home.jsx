97bf384f989f72f798c3f6008c45ef08e189608c
jskartes <joshuakartes@gmail.com>
2024-02-16 12:32:53 -0700

    add copy to home-about

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,12 @@ const Home = () => {
           Software Engineer & Full-Stack Developer
         </span>
       </div>
+
+      <div className='home-about'>
+        <span>Hello, world!</span>
+        <p>I am a recent convert to professional software development, coming from two decades of work as a full-time musician. I'm currently based in beautiful Arvada, Colorado.</p>
+        <p>I am fueled by boundless creativity ... and coffee.</p>
+      </div>
     </div>
   );
 }
